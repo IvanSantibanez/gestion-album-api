3379d513c31e461fb686c19534127a5a1002d4f1
Iván Santibáñez <ivan.t.asc@gmail.com>
2025-12-15 22:44:02 -0300

    first commit

---
+++ src/main/java/cl/ipss/gestion_album_api/GestionAlbumApiApplication.java
@@ -0,0 +1,13 @@
+package cl.ipss.gestion_album_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionAlbumApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionAlbumApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/ipss/gestion_album_api/GestionAlbumApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.ipss.gestion_album_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionAlbumApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
